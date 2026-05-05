0e87cfba2b8605d70b5bf9ebe7431a2c0fba031c
user131943902 <lulzmynewaccount@proton.me>
2026-05-05 16:15:31 +0000

    Update config.h

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -66,7 +66,7 @@ static Sp scratchpads[] = {
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8" };
 
 static const unsigned int ulinepad		= 5;	/* horizontal padding between the underline and tag */
-static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinestroke	= 1;	/* thickness / height of the underline */
 static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
 static const int ulineall			= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
